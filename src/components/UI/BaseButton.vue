bc69a2b4a1e1a2ffa16ff237c91e23617742c68e
Martin Fesneau <martin.fesneau@gmail.com>
2020-11-25 14:41:15 +0100

    first commit for rememberme app

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button :type="type" :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['type', 'mode']
+}
+</script>
+
+<style scoped>
+  button {
+    font-weight: bold;
+    background-color: #222831;
+    color: #ffd369;
+    padding: 0.75rem 1rem;
+    border-radius: 25px;
+    text-decoration: none;
+    font-family: inherit;
+    border: 1px solid #222831;
+    cursor: pointer;
+      outline-style: none;
+}
+
+button:hover,
+button:active {
+  background-color: #393e46;
+  border-color: #393e46;
+}
+
+.flat {
+  background-color: transparent;
+  color: #393e46;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #ffd369;
+}
+</style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['title']
+};
+</script>
+<style scoped>
+header {
+  width: 100%;
+  background-color: #222831;
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  color: #ffd369;
+  margin: 0;
+}
+</style>
