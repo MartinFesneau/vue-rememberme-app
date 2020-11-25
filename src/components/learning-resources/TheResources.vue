e42d23e987772eb7531489d9e4e1ca01c856e16e
Martin Fesneau <martin.fesneau@gmail.com>
2020-11-25 16:35:24 +0100

    add resources and dialog error

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,75 @@
+<template>
+  <base-card>
+    <base-button 
+      @click="setSelectedTab('stored-resources')"
+      :mode="storeResButtonMode"
+      >Stored Resources</base-button>
+    <base-button
+    @click="setSelectedTab('add-resources')"
+    :mode="addResButtonMode"
+    >Add resources</base-button>
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab" @add-new-resource="addResource"></component>
+  </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResources from './AddResources.vue'
+
+export default {
+  components: {
+    StoredResources,
+    AddResources
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedRessources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedRessources,
+      addResource: this.addResource,
+    }
+  },
+  computed: {
+    storeResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat'
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resources' ? null : 'flat'
+    }
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      const newRes = {
+        id: new Date().toISOString,
+        title,
+        description,
+        link
+      };
+      this.storedRessources.unshift(newRes);
+      this.selectedTab = 'stored-resources'
+    }
+  }
+}
+</script>
